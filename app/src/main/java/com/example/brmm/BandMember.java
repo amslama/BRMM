dd1f9fe2dff000900cf1c9d01efd605f27878f68
Sievert, Benjamin <bcsiev2@ilstu.edu>
2019-11-25 17:58:06 -0600

    Expansion of BandMember Class

--- app/src/main/java/com/example/brmm/BandMember.java
+++ app/src/main/java/com/example/brmm/BandMember.java
@@ -1,7 +1,15 @@
 package com.example.brmm;
 
+import java.util.ArrayList;
+
 public abstract class BandMember
 {
-    private String fname;
-    private String lname;
+    protected String fname;
+    protected String lname;
+    protected String ulid;
+    protected int UID;
+    protected String passWord;
+    protected ArrayList<Rentable> instruments;
+    protected ArrayList<Rentable> parts;
+
 }
