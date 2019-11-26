7667346a13a13de99b5a9fc9603f584e4d25e0ca
Sievert, Benjamin <bcsiev2@ilstu.edu>
2019-11-25 18:03:41 -0600

    Started checkout class

---
+++ app/src/main/java/com/example/brmm/CheckoutItems.java
@@ -0,0 +1,12 @@
+package com.example.brmm;
+
+import java.util.Date;
+
+public class CheckoutItems {
+    private Date transActionDate;
+    private Date dueDate;
+    private double totalCosts;
+    private BandMember assignedBandMember;
+
+    public void completeTransaction(){}
+}
