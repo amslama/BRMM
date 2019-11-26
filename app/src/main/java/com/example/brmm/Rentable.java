a098ee0e197f80747340557c57b9cb201d026e86
Sievert, Benjamin <bcsiev2@ilstu.edu>
2019-11-25 18:27:21 -0600

    Constructor for Rentable

--- app/src/main/java/com/example/brmm/Rentable.java
+++ app/src/main/java/com/example/brmm/Rentable.java
@@ -8,4 +8,12 @@ public abstract class Rentable
     protected BandMember owner;
     protected String notes;
     protected Category highestCategory;
+
+    public Rentable(String name, double cost, BandMember owner, String notes, Category highestCategory) {
+        this.name = name;
+        this.cost = cost;
+        this.owner = owner;
+        this.notes = notes;
+        this.highestCategory = highestCategory;
+    }
 }
