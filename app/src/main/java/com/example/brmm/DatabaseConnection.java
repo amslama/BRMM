592847498cac93be8b531b18fbeddbbaf0847007
unknown <bjsmit1@ilstu.edu>
2019-11-26 21:24:08 -0600

    Created add and remove faculty rights for users

--- app/src/main/java/com/example/brmm/DatabaseConnection.java
+++ app/src/main/java/com/example/brmm/DatabaseConnection.java
@@ -1,177 +1,166 @@
-        package com.example.brmm;
+package com.example.brmm;
 
-        import java.sql.Connection;
-        import java.sql.DriverManager;
-        import java.sql.PreparedStatement;
-        import java.sql.ResultSet;
-        import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-        import com.jcraft.jsch.JSch;
-        import com.jcraft.jsch.Session;
-        import com.jcraft.jsch.UserAuth;
-        import com.jcraft.jsch.UserInfo;
-        import java.sql.DriverManager;
-        import java.sql.PreparedStatement;
-        import java.sql.ResultSet;
-        import java.sql.SQLException;
-        import java.sql.Statement;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.UserAuth;
+import com.jcraft.jsch.UserInfo;
 
-        public class DatabaseConnection extends Thread
-        {
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-        private int portNum;
-        private final String SSH_Host = "10.110.21.210";
-        private final String SSH_User = "vmuser";
-        private final String SSH_Pass = "brmmproject4!";
-        private final String MySQL_Host = "127.0.0.1";
-        private final String MySQL_User = "vmuser";
-        private final String MySQL_Pass = "brmmproject4!";
-        private Connection conn = null;
+public class DatabaseConnection extends Thread {
 
-        public void run(){
+    private int portNum;
+    private final String SSH_Host = "10.110.21.210";
+    private final String SSH_User = "vmuser";
+    private final String SSH_Pass = "brmmproject4!";
+    private final String MySQL_Host = "127.0.0.1";
+    private final String MySQL_User = "vmuser";
+    private final String MySQL_Pass = "brmmproject4!";
+    private Connection conn = null;
+
+    public void run() {
         dbGetConnection();
-        }
+    }
 
-        public Session getServerConnection() {
+    public Session getServerConnection() {
         //Session serverSession = null;
         Session serverSession = null;
         try {
-        java.util.Properties config = new java.util.Properties();
-        config.put("StrictHostKeyChecking", "no");
-        JSch jsch = new JSch();
-        serverSession = jsch.getSession(SSH_User, SSH_Host, 22);
-        serverSession.setPassword(MySQL_Pass);
-        serverSession.setConfig(config);
-        System.out.println("Starting SSH Connection");
-        System.out.println(serverSession.getHost() + "\n" + serverSession.getUserName() + "\n" + serverSession.getPort());
-        System.out.println("Connection Established");
-        serverSession.connect();
-        System.out.println("Connected = " + serverSession.isConnected());
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no");
+            JSch jsch = new JSch();
+            serverSession = jsch.getSession(SSH_User, SSH_Host, 22);
+            serverSession.setPassword(MySQL_Pass);
+            serverSession.setConfig(config);
+            System.out.println("Starting SSH Connection");
+            System.out.println(serverSession.getHost() + "\n" + serverSession.getUserName() + "\n" + serverSession.getPort());
+            System.out.println("Connection Established");
+            serverSession.connect();
+            System.out.println("Connected = " + serverSession.isConnected());
 
-        //Port Forwarding stuff
-        int assigned_port = serverSession.setPortForwardingL(5656, MySQL_Host, 3306);
-        System.out.println("localhost:" + assigned_port + "->" + MySQL_Host + ":" + 3306);
-        System.out.println("Port Forwarded");
-
-        //DB Part
-        //System.out.println("Before the Driver");
-        //Class.forName("com.mysql.jdbc.Driver").newInstance();
-        //System.out.println("After the Driver.......................");
-        //Getting Stuck Right Here
-
-        //serverConn = DriverManager.getConnection("jdbc:mysql://localhost:4321/BRMM/  ?autoReconnect=true&amp;useSSL=false",MySQL_User,MySQL_Pass);
-        //System.out.println("Database connection established");
-
-
+            //Port Forwarding stuff
+            int assigned_port = serverSession.setPortForwardingL(5656, MySQL_Host, 3306);
+            System.out.println("localhost:" + assigned_port + "->" + MySQL_Host + ":" + 3306);
+            System.out.println("Port Forwarded");
         } catch (Exception e) {
-        e.printStackTrace();
+            e.printStackTrace();
         }
         return serverSession;
-        }
+    }
 
 
-        public void dbGetConnection(){
+    public void dbGetConnection() {
         Session ses = null;
         try {
-        ses = getServerConnection();
-        }
-        catch(Exception v){
-        System.out.println("Part 2 fail...................");
-        }
-
-        try{
-        Class.forName("com.mysql.jdbc.Driver");
-        String url = "jdbc:mysql://localhost:5656/BRMM";
-        conn = DriverManager.getConnection(url,"vmuser","brmmproject4!");
-        System.out.println("Connection Complete");
-
-        }
-        catch(Exception t) {
-        System.out.println("Part 3 fail...............");
-        t.printStackTrace();
+            ses = getServerConnection();
+        } catch (Exception v) {
+            System.out.println("Part 2 fail...................");
         }
 
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:5656/BRMM";
+            conn = DriverManager.getConnection(url, "vmuser", "brmmproject4!");
+            System.out.println("Connection Complete");
 
-
+        } catch (Exception t) {
+            System.out.println("Part 3 fail...............");
+            t.printStackTrace();
         }
+    }
 
-        //TODO: ADD AND REMOVE RIGHTS
-        public void removeFacultyRights(){
-
+    //TODO: ADD AND REMOVE RIGHTS
+    public void removeFacultyRights(String ulid) {
+        try {
+            String query = "update user set faculty = 0 where username = '"+ulid+"'";
+            Statement st = conn.createStatement();
+            st.executeUpdate(query);
+        } catch (Exception e) {
+            System.out.println("Error on remove Faculty rights");
         }
+    }
 
-        public void addFacultyRights(){
-                try {
-                        String query = "";
-                }
-                catch(Exception e){
-                        System.out.println("Error on add rights");
-                }
+    public void addFacultyRights(String ulid) {
+        try {
+            String query = "update user set faculty = 1 where username = '"+ulid+"'";
+            Statement st = conn.createStatement();
+            st.executeUpdate(query);
+        } catch (Exception e) {
+            System.out.println("Error on add Faculty rights");
         }
+    }
 
-        public boolean checklogin(String ulid, String password){
-                boolean login = false;
-                try{
-                        String ulidquery = "Select username,passwrd from user where username = '"+ulid+"'";
-                        Statement st = conn.createStatement();
-                        ResultSet rs = st.executeQuery(ulidquery);
-                        if(rs.next()&&password.equals(rs.getString("passwrd"))) {
-                                login = true;
-                        }
+    //Login authentication
+    //TODO: ADD ENCRYPTION
+    public boolean checklogin(String ulid, String password) {
+        boolean login = false;
+        try {
+            String ulidquery = "Select username,passwrd from user where username = '" + ulid + "'";
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(ulidquery);
+            if (rs.next() && password.equals(rs.getString("passwrd"))) {
+                login = true;
+            }
 
-                }
-                catch (Exception e){
-                        System.out.println("Error on checking login");
-                }
-                System.out.println(login);
-                return login;
+        } catch (Exception e) {
+            System.out.println("Error on checking login");
         }
+        System.out.println(login);
+        return login;
+    }
 
 
-
-
-        public void test(){
-                try {
-                        String query = "SELECT * FROM item";
-                        Statement st = conn.createStatement();
-                        ResultSet rs = st.executeQuery(query);
-                        while (rs.next()){
-                                System.out.println("Name: "+rs.getString("name"));
-                        }
-                }
-                catch (Exception e){
-                        System.out.println("Error on Test");
-                }
+    public void test() {
+        try {
+            String query = "SELECT * FROM item";
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                System.out.println("Name: " + rs.getString("name"));
+            }
+        } catch (Exception e) {
+            System.out.println("Error on Test");
         }
+    }
 
 
-        public String getSSH_Host() {
+    public String getSSH_Host() {
         return SSH_Host;
-        }
+    }
 
-        public String getSSH_Pass() {
+    public String getSSH_Pass() {
         return SSH_Pass;
-        }
+    }
 
-        public String getMySQL_Host() {
+    public String getMySQL_Host() {
         return MySQL_Host;
-        }
+    }
 
-        public String getMySQL_User() {
+    public String getMySQL_User() {
         return MySQL_User;
-        }
+    }
 
 
-        public String getMySQL_Pass() {
+    public String getMySQL_Pass() {
         return MySQL_Pass;
-        }
+    }
 
-        public int getPortNum() {
+    public int getPortNum() {
         return portNum;
-        }
+    }
 
-        public void setPortNum(int portNum) {
+    public void setPortNum(int portNum) {
         this.portNum = portNum;
-        }
-        }
+    }
+}
 
