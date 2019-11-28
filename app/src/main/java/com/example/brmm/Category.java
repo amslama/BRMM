5b6543b4e1e987c22e53cf99cbdec88c90610b46
Sievert, Benjamin <bcsiev2@ilstu.edu>
2019-11-28 09:00:20 -0600

    Category class

--- app/src/main/java/com/example/brmm/Category.java
+++ app/src/main/java/com/example/brmm/Category.java
@@ -1,4 +1,44 @@
 package com.example.brmm;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Category {
+    private String name;
+    private Category superCategory;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getSuperCategory() {
+        return superCategory;
+    }
+
+    public void setSuperCategory(Category superCategory) {
+        this.superCategory = superCategory;
+    }
+
+
+
+
+    public Category(Category superCategory, String name) {
+        this.superCategory = superCategory;
+        this.name = name;
+
+    }
+
+    public ArrayList<Category> getSuperCategories(Category aCategory) {
+        ArrayList<Category> superCats = new ArrayList<>();
+        Category cat = this;
+        while (aCategory.superCategory!= null) {
+            superCats.add(cat);
+            cat = cat.superCategory;
+        }
+        return superCats;
+    }
 }
