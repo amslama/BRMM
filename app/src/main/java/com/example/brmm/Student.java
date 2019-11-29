e02afb401d5172b98bc881d6e0490d6dfcb81c2d
Slama, Aaron <amslama@ilstu.edu>
2019-11-29 09:16:29 -0600

    UI work

--- app/src/main/java/com/example/brmm/Student.java
+++ app/src/main/java/com/example/brmm/Student.java
@@ -1,8 +1,8 @@
 package com.example.brmm;
 
 public class Student extends BandMember {
-    private String Section;
+    private String section;
     private boolean sectionLeader;
     private int UID;
-
+    private String notes;
 }
