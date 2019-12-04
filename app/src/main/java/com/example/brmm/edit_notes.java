8f4a5f1c038ddcf00259f22c46e6c9c714e5c500
Sievert, Benjamin <bcsiev2@ilstu.edu>
2019-12-03 23:19:10 -0600

    added stuff to edit_notes

--- app/src/main/java/com/example/brmm/edit_notes.java
+++ app/src/main/java/com/example/brmm/edit_notes.java
@@ -2,11 +2,13 @@ package com.example.brmm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class edit_notes extends AppCompatActivity {
+    private String notes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +19,24 @@ public class edit_notes extends AppCompatActivity {
         TextView header = findViewById(R.id.edit_notes_textview);
 
         //Edittext
-        EditText notes_edittext = findViewById(R.id.edit_notes_edittext);
+        final EditText notes_edittext = findViewById(R.id.edit_notes_edittext);
 
         //Buttons
         Button ok_button = findViewById(R.id.ok_edit_notes_button);
         Button cancel_button = findViewById(R.id.cancel_edit_notes_button);
+
+        ok_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                notes = notes_edittext.getText().toString();
+            }
+        });
+
+        cancel_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                notes_edittext.setText("");
+            }
+        });
     }
 }
