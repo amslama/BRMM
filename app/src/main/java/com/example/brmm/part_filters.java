4f30fa757a9eef176aaffbb44e17d38e58807fb7
Sievert, Benjamin <bcsiev2@ilstu.edu>
2019-12-04 23:07:54 -0600

    parts lists explicitly deal with parts

--- app/src/main/java/com/example/brmm/part_filters.java
+++ app/src/main/java/com/example/brmm/part_filters.java
@@ -1,12 +1,19 @@
+
+
 package com.example.brmm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.example.brmm.R;
+import com.example.brmm.Part;
 
 import java.util.ArrayList;
 
@@ -25,9 +32,9 @@ public class part_filters extends AppCompatActivity {
         TextView cost_textview = findViewById(R.id.name_part_filters_textview);
 
         //Edittexts
-        EditText name_edittext = findViewById(R.id.name_part_filters_edittext);
-        EditText sn_edittext = findViewById(R.id.sn_part_filters_edittext);
-        EditText cost_edittext = findViewById(R.id.cost_part_filters_edittext);
+        final EditText name_edittext = findViewById(R.id.name_part_filters_edittext);
+        final EditText sn_edittext = findViewById(R.id.sn_part_filters_edittext);
+        final EditText cost_edittext = findViewById(R.id.cost_part_filters_edittext);
 
         //Dropdowns
         final Spinner compwith_spin = findViewById(R.id.compwith_part_filters_dropdown);
@@ -40,9 +47,88 @@ public class part_filters extends AppCompatActivity {
         final Button cancel_button = findViewById(R.id.cancel_part_filters_button);
         final Button apply_button = findViewById(R.id.apply_part_filters_button);
 
+        cancel_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name_edittext.setText("");
+                sn_edittext.setText("");
+                cost_edittext.setText("");
+            }
+        });
+
+        apply_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name;
+                int sn;
+                double cost;
+
+                name = name_edittext.getText().toString();
+                try {sn = Integer.parseInt(sn_edittext.getText().toString());}
+                catch (NumberFormatException ex) {sn = 0;}
+
+                try {
+                    cost = Double.parseDouble(cost_edittext.getText().toString());
+                } catch (NumberFormatException ex) {cost = -1;}
+
+                ArrayList<Part> parts = filterParts(null, name, sn, cost);
+                Intent intent = new Intent();
+                intent.putExtra("partList", parts);
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        });
+
 
     }
+    public   ArrayList<Part> filterParts(ArrayList<Part> aList, String name, int sn, double cost) {
+        ArrayList<Part> filter = aList;
+        if (!name.equals(""))
+            filter =  filterByName(filter, name);
+
+        if (sn != 0)
+            filter = filterBySn(filter,sn);
+
+        if (cost != -1)
+            filter = filterByCost(filter,cost);
 
 
+
+        return  filter;
+    }
+
+    public ArrayList<Part> filterByName(ArrayList<Part> parts, String name) {
+        ArrayList<Part> filter = new ArrayList<>();
+        for (Part aPart: parts) {
+            if (aPart.getName().equals(name)) {
+                filter.add(aPart);
+                break;
+            }
+        }
+        return filter;
+    }
+
+    public ArrayList<Part> filterBySn(ArrayList<Part> parts, int sn) {
+        ArrayList<Part> filter = new ArrayList<>();
+        for (Part aPart: parts) {
+            if (aPart instanceof Part) {
+                if (((Part) aPart).getSerialNumber() == sn) {
+                    filter.add(aPart);
+                    break;
+                }
+
+            }
+        }
+        return filter;
+    }
+
+    public ArrayList<Part> filterByCost(ArrayList<Part> parts, double cost) {
+        ArrayList<Part> filter = new ArrayList<>();
+        for (Part aPart: parts) {
+            if (aPart.getCost() == cost)
+                filter.add(aPart);
+        }
+        return filter;
+    }
 
 }
